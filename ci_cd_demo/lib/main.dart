55075507685ccef350e788a5c3cc7234531ca40e
arthurgiani <arthur@getleslie.com>
2022-06-21 10:01:13 -0300

    ci-cd-trigger-test

--- ci_cd_demo/lib/main.dart
+++ ci_cd_demo/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo CI/CD',
+      title: 'Flutter Demo CI/CD 2',
       theme: ThemeData(
         // This is the theme of your application.
         //
